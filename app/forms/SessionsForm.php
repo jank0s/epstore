75b102622db77d1ea68c7c782d02594e2f4d9cab
Jan Kos <jan@k0s.si>
2016-12-20 21:35:13 +0100

    Login form bootstrap attributes

--- app/forms/SessionsForm.php
+++ app/forms/SessionsForm.php
@@ -7,6 +7,8 @@ require_once 'HTML/QuickForm2/Element/InputText.php';
 require_once 'HTML/QuickForm2/Element/Textarea.php';
 require_once 'HTML/QuickForm2/Element/InputCheckbox.php';
 require_once 'HTML/QuickForm2/Element/InputPassword.php';
+require_once 'HTML/QuickForm2/Element/Button.php';
+
 
 abstract class SessionsAbstractForm extends HTML_QuickForm2 {
 
@@ -17,19 +19,26 @@ abstract class SessionsAbstractForm extends HTML_QuickForm2 {
     public function __construct($id) {
         parent::__construct($id);
 
+        $this->setAttribute('class', 'form-signin');
+
         $this->email = new HTML_QuickForm2_Element_InputText('email');
-        $this->email->setAttribute('size', 70);
         $this->email->setLabel('Email');
-        $this->email->addRule('required', 'Vpisi email');
+        $this->email->addRule('required', 'Manjka email');
+        $this->email->setAttribute('class', 'form-control');
+        $this->email->setAttribute('placeholder', 'Email');
         $this->addElement($this->email);
 
         $this->password = new HTML_QuickForm2_Element_InputPassword('password');
-        $this->password->setAttribute('size', 70);
         $this->password->setLabel('Geslo');
-        $this->password->addRule('required', 'Vpisi geslo');
+        $this->password->addRule('required', 'Manjka geslo');
+        $this->password->setAttribute('class', 'form-control');
+        $this->password->setAttribute('placeholder', 'Geslo');
         $this->addElement($this->password);
 
-        $this->button = new HTML_QuickForm2_Element_InputSubmit(null);
+        $this->button = new HTML_QuickForm2_Element_Button();
+        $this->button->setAttribute('class', 'btn btn-lg btn-primary btn-block');
+        $this->button->setAttribute('type', 'submit');
+        $this->button->setValue('Prijava');
         $this->addElement($this->button);
 
         $this->addRecursiveFilter('trim');
