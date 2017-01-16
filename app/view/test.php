57cb92ce35b1a79f205e265e276df487050e1ec7
hamihaa <miha.babic@hotmail.com>
2017-01-16 18:47:53 +0100

    Delete test.php

--- app/view/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-# Opens file log.txt
-	$log = fopen('/var/log/epstore/log.txt', "a+");
-        fputs($log, date("[d.m.Y, H:i:s]") .
-                " user ID: " . $user . " " . $action .
-                "\n");
-	fclose($log);
